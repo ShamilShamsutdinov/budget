c460860cdcb764f21a3f69380fbcc96cd43f3565
ShamilShamsutdinov <indahoodshop@mail.ru>
2026-01-18 16:36:50 +0600

    Добавил zod на бэк

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,8 +1,9 @@
 import { initTRPC } from '@trpc/server'
+import { z } from 'zod'
 
 const transactions = [
-  { id: '1', name: 'Shamil', transaction: '25 000р', category: 'Еда' },
-  { id: '2', name: 'Shamil', transaction: '50 000р', category: 'Развлечения' }
+  { id: '1', name: 'Shamil', transaction: '25 000р', category: 'Еда', comment: 'Сходили в ресторан' },
+  { id: '2', name: 'Shamil', transaction: '50 000р', category: 'Развлечения', comment: '' }
 ]
 
 const trpc = initTRPC.create()
@@ -10,6 +11,16 @@ const trpc = initTRPC.create()
 export const trpcRouter = trpc.router({
   getTransactions: trpc.procedure.query(() => {
     return { transactions }
+  }),
+  getTransaction: trpc.procedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(({ input }) => {
+      const transaction = transactions.find((transaction) => transaction.id === input.id)
+      return { transaction: transaction || null }
   }),
 })
 
--- webapp/src/pages/ViewTransactionPage/index.tsx
+++ webapp/src/pages/ViewTransactionPage/index.tsx
@@ -1,14 +1,31 @@
 import { useParams } from "react-router-dom"
 import type { ViewTransactionRouteParams } from "../../lib/routes"
+import { trpc } from "../../lib/trpc"
 
 export const ViewTransactionPage = () => {
   const { id } = useParams() as ViewTransactionRouteParams
+
+  const { data, error, isLoading, isFetching, isError } = trpc.getTransaction.useQuery({
+    id
+  })
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+  if (!data?.transaction) {
+    return <span>Idea not found</span>
+  }
+
   return (
     <div>
-      <h1>{id}</h1>
-      <p>15 000р</p>
-      <p>Развлечения</p>
-      <p>Сходили в ресторан</p>
+      <p>{data.transaction.transaction}</p>
+      <p>{data.transaction.category}</p>
+      <p>{data.transaction.comment}</p>
     </div>
   )
 }
